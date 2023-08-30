4660b1145660e9e210cada3abae5e2c9f85dc125
walesyu <wales520@gmail.com>
2023-08-30 21:45:10 +0800

    chore: test deploy

--- deploy/ghpages.js
+++ deploy/ghpages.js
@@ -3,7 +3,7 @@ const ghpages = require('gh-pages');
 
 const options = {
   branch: 'gh-pages',
-  repo: 'https://github.com/andrew781026/F2E_Card.git' // project github repo
+  repo: 'https://github.com/walesyu/vue-exercise.git' // project github repo
 };
 
 const callback = err => {
